f483246176d906b443388de89b8042df8d756239
R4RISHAB <rishabbassi800@gmail.com>
2026-06-20 10:40:06 +0000

    conatiner confg

---
+++ JS BASICS/variables.js
@@ -0,0 +1,9 @@
+const accountID = 142233
+let accountEmail = 'RISHAB123@gmail.com'
+var accountpass ='123'
+accountcity ="ludhiana"
+
+// accountID = 2 // not allowed bcz this accountid is in the const variable
+console.log(accountID);
+
+ 
